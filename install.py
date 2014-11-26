9f47176b0ca50dd5cc654502479b4dbb705bfd3c
brewster76 <nick.dajda@gmail.com>
2014-11-26 16:26:36 +0000

    Install script and instructions updated for new templates.  Bootstrap css and js support files added to distribution.

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
-# installer for bootstrap
+# installer for the bootstrap skin.
 #
-# Based on installer for xstats (Copyright 2014 Matthew Wall)
+# Based on installer for xstats
 #
 # 27 March 2014
 # Configured by Nick to install bootstrap skin.
@@ -13,30 +13,30 @@ def loader():
 class BootstrapInstaller(ExtensionInstaller):
     def __init__(self):
         super(BootstrapInstaller, self).__init__(
-            version="1.01",
+            version="2.1",
             name='bootstrap',
-            description='A skin based around the bootstrap framework',
+            description='A skin based around the bootstrap 3.2.0 framework',
             author="Nick Dajda",
             author_email="nick.dajda@gmail.com",
             config={
                 'StdReport': {
                     'SmallImages': {
                         'skin':'Images',
-                        'HTML_ROOT':'Bootstrap'},
+                        'HTML_ROOT':'public_html/Bootstrap'},
                     'BigImages': {
                         'skin':'Images',
-                        'HTML_ROOT':'Bootstrap/big_images',
+                        'HTML_ROOT':'public_html/Bootstrap/big_images',
                         'ImageGenerator' : {
                             'image_width' : '800',
                             'image_height' : '500'}},
                     'HTMLPages': {
                         'skin':'Bootstrap',
-                        'HTML_ROOT':'Bootstrap'}}},
+                        'HTML_ROOT':'public_html/Bootstrap'}}},
 
             files=[('skins/Bootstrap',
-                    ['skins/Bootstrap/index.html.tmpl',
-                     'skins/Bootstrap/about.html.tmpl',
+                    ['skins/Bootstrap/about.html.tmpl',
                      'skins/Bootstrap/history.html.tmpl',
+                     'skins/Bootstrap/index.html.tmpl',
                      'skins/Bootstrap/month.html.tmpl',
                      'skins/Bootstrap/news.html.tmpl',
                      'skins/Bootstrap/stats.html.tmpl',
@@ -45,31 +45,17 @@ class BootstrapInstaller(ExtensionInstaller):
                      'skins/Bootstrap/skin.conf']),
                    ('skins/Bootstrap/NOAA',
                     ['skins/Bootstrap/NOAA/NOAA-YYYY.html.tmpl',
-                     'skins/Bootstrap/NOAA/NOAA-YYYY-MM.txt.tmpl',
-                     'skins/Bootstrap/NOAA/NOAA-YYYY.txt.tmpl']),
+                     'skins/Bootstrap/NOAA/NOAA-YYYY-MM.txt.tmpl']),
                    ('skins/Images', 
                     ['skins/Images/skin.conf']),
                    ('bin/user',
                     ['bin/user/gaugeengine.py',
                      'bin/user/gauges.py',
                      'bin/user/historygenerator.py']),
-                   ('public_html/Bootstrap/assets/css',
-                    ['public_html/Bootstrap/assets/css/bootstrap.css',
-                     'public_html/Bootstrap/assets/css/bootstrap.min.css',
-                     'public_html/Bootstrap/assets/css/bootstrap-responsive.css',
-                     'public_html/Bootstrap/assets/css/bootstrap-responsive.min.css',
-                     'public_html/Bootstrap/assets/css/lightbox.css']),
-                   ('public_html/Bootstrap/assets/img',
-                    ['public_html/Bootstrap/assets/img/close.png',
-                     'public_html/Bootstrap/assets/img/glyphicons-halflings.png',
-                     'public_html/Bootstrap/assets/img/glyphicons-halflings-white.png',
-                     'public_html/Bootstrap/assets/img/loading.gif',
-                     'public_html/Bootstrap/assets/img/next.png',
-                     'public_html/Bootstrap/assets/img/prev.png']),
-                   ('public_html/Bootstrap/assets/js',
-                    ['public_html/Bootstrap/assets/js/bootstrap.min.js',
-                     'public_html/Bootstrap/assets/js/jquery-1.7.2.min.js',
-                     'public_html/Bootstrap/assets/js/lightbox.js'])
+                   ('public_html/Bootstrap/css',
+                    ['public_html/Bootstrap/css/bootstrap.min.css']),
+                   ('public_html/Bootstrap/js',
+                    ['public_html/Bootstrap/js/bootstrap.min.js'])
                    ]
             )
 
