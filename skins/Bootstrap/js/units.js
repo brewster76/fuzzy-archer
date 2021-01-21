0ef7650e695f77fe5049edf9d592b49d4ef44e3c
Michi <michael@kainzbauer.net>
2021-01-21 22:26:31 +0100

    conversions, languages

--- skins/Bootstrap/js/units.js
+++ skins/Bootstrap/js/units.js
@@ -142,7 +142,214 @@ function convert(itemConfig, value) {
     //console.log("Couldn't find conversion function: '" + functionName + "', returning value without conversion");
     return value;
 }
+//group_altitude
+function meter_To_foot(value) {
+    return value * 3.280839895013123;
+}
+function foot_To_meter(value) {
+    return value * 0.3048;
+}
+//group_degree_day
+function degree_F_day_To_degree_C_day(value) {
+    return degree_F_To_degree_C(value);
+}
+function degree_C_day_To_degree_F_day(value) {
+    return degree_C_To_degree_F(value);
+}
+//group_distance
+function mile_To_km(value) {
+    return value * 1.609344;
+}
+function km_To_mile(value) {
+    return value * 0.621371192237334;
+}
+//group_pressure
+function inHg_To_mmHg(value) {
+    return inch_To_mm(value);
+}
+function inHg_To_mbar(value) {
+    return value * 33.8638815789;
+}
+function inHg_To_hPa(value) {
+    return inHg_To_mbar(value);
+}
+function inHg_To_kPa(value) {
+    return hPa_To_kPa(inHg_To_hPa(value));
+}
 
+function mmHg_To_inHg(value) {
+    return mm_To_inch(value);
+}
+function mmHg_To_mbar(value) {
+    return value * 1.33322368421;
+}
+function mmHg_To_hPa(value) {
+    return mmHg_To_mbar(value);
+}
+function mmHg_To_kPa(value) {
+    return hPa_To_kPa(mmHg_To_hPa(value));
+}
+
+function mbar_To_inHg(value) {
+    return value * 0.029529987508079485;
+}
+function mbar_To_mmHg(value) {
+    return value * 0.7500616827044659;
+}
+function mbar_To_hPa(value) {
+    return value;
+}
+function mbar_To_kPa(value) {
+    return hPa_To_kPa(hPa_To_mbar(value));
+}
+
+function hPa_To_inHg(value) {
+    return mbar_To_inHg(value);
+}
+function hPa_To_mmHg(value) {
+    return mbar_To_mmHg(value);
+}
+function hPa_To_mbar(value) {
+    return value;
+}
+function hPa_To_kPa(value) {
+    return value * 0.1;
+}
+
+function kPa_To_inHg(value) {
+    return value * 0.29529987508079486;
+}
+function kPa_To_mmHg(value) {
+    return value * 7.5006168270446585;
+}
+function kPa_To_mbar(value) {
+    return value * 10;
+}
+function kPa_To_hPa(value) {
+    return hPa_To_kPa(value);
+}
+//group_rain
+function inch_To_mm(value) {
+    return value * 25.3999999999745;
+}
+function inch_To_cm(value) {
+    return value * 2.53999999999745;
+}
+function mm_To_cm(value) {
+    return value * 0.1;
+}
+function mm_To_inch(value) {
+    return mm_To_cm(cm_To_inch(value));
+}
+function cm_To_mm(value) {
+    return value * 10;
+}
+function cm_To_inch(value) {
+    return value * 0.3937007874015748;
+}
+//group_rainrate
+function inch_per_hour_To_mm_per_hour(value) {
+    return inch_To_mm(value);
+}
+function inch_per_hour_To_cm_per_hour(value) {
+    return inch_To_cm(value);
+}
+function mm_per_hour_To_cm_per_hour(value) {
+    return mm_To_cm(value);
+}
+function mm_per_hour_To_inch_per_hour(value) {
+    return mm_To_inch(value);
+}
+function cm_per_hour_To_mm_per_hour(value) {
+    return cm_To_mm(value);
+}
+function cm_per_hour_To_inch_per_hour(value) {
+    return cm_To_inch(value);
+}
+//group_speed
 function meter_per_second_To_km_per_hour(value) {
     return value * 3.6;
+}
+function meter_per_second_To_mile_per_hour(value) {
+    return value * 2.2369362920544025;
+}
+function meter_per_second_To_knot(value) {
+    return value * 1.94384;
+}
+
+function km_per_hour_To_meter_per_second(value) {
+    return value / 3.6;
+}
+function km_per_hour_To_mile_per_hour(value) {
+    return km_To_mile(value);
+}
+function km_per_hour_To_knot(value) {
+    return value * 0.539957;
+}
+
+function mile_per_hour_To_meter_per_second(value) {
+    return value * 0.44704;
+}
+function mile_per_hour_To_km_per_hour(value) {
+    return mile_To_km(value);
+}
+function mile_per_hour_To_knot(value) {
+    return value * 0.868976;
+}
+
+function knot_To_meter_per_second(value) {
+    return value * 0.514444;
+}
+function knot_To_km_per_hour(value) {
+    return value * 1.852;
+}
+function knot_To_mile_per_hour(value) {
+    return value * 1.15078;
+}
+//group_speed2
+function meter_per_second2_To_km_per_hour2(value) {
+    return meter_per_second_To_km_per_hour(value);
+}
+function meter_per_second2_To_mile_per_hour2(value) {
+    return meter_per_second_To_mile_per_hour(value);
+}
+function meter_per_second2_To_knot2(value) {
+    return meter_per_second_To_knot(value);
+}
+
+function km_per_hour2_To_meter_per_second2(value) {
+    return km_per_hour_To_meter_per_second(value);
+}
+function km_per_hour2_To_mile_per_hour2(value) {
+    return km_per_hour_To_mile_per_hour(value);
+}
+function km_per_hour2_To_knot2(value) {
+    return km_per_hour_To_knot(value);
+}
+
+function mile_per_hour2_To_meter_per_second2(value) {
+    return mile_per_hour_To_meter_per_second(value);
+}
+function mile_per_hour2_To_km_per_hour2(value) {
+    return mile_per_hour_To_km_per_hour(value);
+}
+function mile_per_hour2_To_knot2(value) {
+    return mile_per_hour_To_knot(value);
+}
+
+function knot2_To_meter_per_second2(value) {
+    return knot_To_meter_per_second(value);
+}
+function knot2_To_km_per_hour2(value) {
+    return knot_To_km_per_hour(value);
+}
+function knot2_To_mile_per_hour2(value) {
+    return knot_To_mile_per_hour(value);
+}
+//group_temperature
+function degree_F_To_degree_C(value) {
+    return (value - 32) * 5 / 9;
+}
+function degree_C_To_degree_F(value) {
+    return (value * 9 / 5) + 32;
 }
