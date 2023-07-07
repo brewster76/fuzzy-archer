3102b7439d67cdde0010e9305772c903505d8b65
Michi <michael@kainzbauer.net>
2023-07-07 18:43:19 +0200

    fixed scatter chart, made items of Stats, HistoryReport, Gauges, Charts, ... configurable by ordered list

--- bin/user/jsonengine.py
+++ bin/user/jsonengine.py
@@ -114,7 +114,7 @@ class JSONGenerator(weewx.reportengine.ReportGenerator):
 
         for gauge in self.gauge_dict.sections:
             data_type = self.gauge_dict[gauge].get('data_type', gauge)
-            ret, gauge_history = self.gen_history_data(gauge, data_type, live_options, self.gauge_dict[gauge].get('data_binding', None))
+            ret, gauge_history = self.gen_history_data(gauge, data_type, live_options, self.gauge_dict[gauge].get('data_binding', None), None)
             self.frontend_data['gauges'][gauge]['target_unit'] = self.get_target_unit(gauge)
             self.frontend_data['gauges'][gauge]['obs_group'] = self.get_obs_group(gauge)
 
@@ -125,7 +125,8 @@ class JSONGenerator(weewx.reportengine.ReportGenerator):
         for chart in self.chart_dict.sections:
             for category in self.chart_dict[chart].sections:
                 data_type = self.chart_dict[chart][category].get('data_type', category)
-                ret, category_history = self.gen_history_data(category, data_type, live_options, self.chart_dict[chart][category].get('data_binding'))
+                plotType = self.chart_dict[chart][category].get('plotType', 'line')
+                ret, category_history = self.gen_history_data(category, data_type, live_options, self.chart_dict[chart][category].get('data_binding'), plotType)
                 self.frontend_data['charts'][chart][category]['target_unit'] = self.get_target_unit(category)
                 self.frontend_data['charts'][chart][category]['obs_group'] = self.get_obs_group(category)
 
@@ -174,7 +175,7 @@ class JSONGenerator(weewx.reportengine.ReportGenerator):
             log.info("JSONGenerator: weewx.units.obs_group_dict['%s'] is not present, using the empty string." % column_name)
             return ""
 
-    def gen_history_data(self, obs_name, column_name, live_options, binding_name):
+    def gen_history_data(self, obs_name, column_name, live_options, binding_name, plotType):
         log.debug("Generating history for obs_name %s and column_name %s with binding %s" % (obs_name, column_name, binding_name))
         if obs_name in self.frontend_data:
             log.debug("Data for observation %s has already been collected." % obs_name)
@@ -208,17 +209,16 @@ class JSONGenerator(weewx.reportengine.ReportGenerator):
             batch_records = db_manager.genBatchRecords(self.lastGoodStamp - timespan * 60 * 60, self.lastGoodStamp)
 
             for rec in batch_records:
-
-
-
-
                 db_value_tuple = weewx.units.as_value_tuple(rec, column_name)
                 if target_unit == "":
                     history_value = rec[column_name]
                 else:
                     history_value = weewx.units.convert(db_value_tuple, target_unit)[0]
                 try:
-                    history_list.append(float(history_value))
+                    if history_value == None and plotType == 'scatter':
+                        history_list.append(history_value)
+                    else:
+                        history_list.append(float(history_value))
                     time_list.append(rec['dateTime'] * 1000)
                 except:
                     log.debug("JSONGenerator: Cannot decode reading of '%s' for column '%s'" % (history_value, column_name))
