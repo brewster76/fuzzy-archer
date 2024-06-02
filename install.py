962c0269e14ad3c1dc0649c82912aa1d316843d7
mKainzbauer <michael@kainzbauer.net>
2024-06-02 17:02:32 +0200

    version

--- install.py
+++ install.py
@@ -93,7 +93,6 @@ class BootstrapInstaller(setup.ExtensionInstaller):
             author="Nick Dajda, Michael Kainzbauer and other contributors",
             author_email="nick.dajda@gmail.com",
             config={
-                'fuzzy_archer_version':version,
                 'StdReport': {
                     'Bootstrap': {
                         'skin':'Bootstrap',
