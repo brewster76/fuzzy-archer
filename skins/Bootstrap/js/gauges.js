3fb66b576c1c1bd4a0f6f9a0b91498c7a70f2879
dcapslock <dcapslock@outlook.com.au>
2024-12-12 19:41:39 +1100

    Parse Boolean Default False does not Honour True

--- skins/Bootstrap/js/gauges.js
+++ skins/Bootstrap/js/gauges.js
@@ -102,8 +102,14 @@ function parseBooleanDefaultFalse(value) {
 }
 
 function parseBoolean(value, defaultValue) {
-    if (value !== undefined && value.toLowerCase() === "false") {
+    if (value !== undefined) {
+        if (value.toLowerCase() === "false") {
         return false;
+        } else if (value.toLowerCase() === "true") {
+            return true;
+        } else {
+            return defaultValue;
+        }
     }
     return defaultValue;
 }
